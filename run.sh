bfadb5610775de9b68c953c60e456934d8948b69
Everton Constantino <everton.constantino@hotmail.com>
2021-05-13 14:48:40 +0000

    WIP 2

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 function run() {
     OLD=0
     NEW=0
+    NEWP=0
     EXECS=$1
     SIZE=$2
     RUNS=$3
@@ -9,25 +10,24 @@ function run() {
         SEL=$(A=$(shuf -i 0-10 -n 1); echo $(($A % 2)))
         if [ $SEL -eq 0 ]; then
             T_OLD=$(./gto $SIZE $RUNS)
-            #echo "Master: $T_OLD"
-            OLD=$OLD+$T_OLD
             T_NEW=$(./gt $SIZE $RUNS)
-            #echo "Current: $T_NEW"
+            T_NEWP=$(./gtp $SIZE $RUNS)
         else
             T_NEW=$(./gt $SIZE $RUNS)
-            #echo "Current: $T_NEW"
+            T_NEWP=$(./gtp $SIZE $RUNS)
             T_OLD=$(./gto $SIZE $RUNS)
-            #echo "Master: $T_OLD"
-            OLD=$OLD+$T_OLD
         fi
         NEW=$NEW+$T_NEW
+        OLD=$OLD+$T_OLD
+        NEWP=$NEWP+$T_NEWP
     done
     SPEED=$(echo "($OLD) / ($NEW)" | bc -l)
-    echo "$SIZE -> $SPEED"
+    SPEEDP=$(echo "($OLD) / ($NEWP)" | bc -l)
+    echo "$SIZE -> $SPEED $SPEEDP"
 }
 
 run $1 16 500
 run $1 32 500
 run $1 64 500
 run $1 128 100
-run $1 256 100
+run $1 256 50
